bd7cf5bd9691f5e725fa3415c011b57d45fa6af0
eric <ericshell2010@gmail.com>
2016-04-22 03:54:48 -0400

    refactor

--- server/todos.js
+++ server/todos.js
@@ -10,6 +10,12 @@ app.set('trust proxy');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+//     next();
+// });
 
 app.use('/api/todos',require('./routes/todos'));
 
