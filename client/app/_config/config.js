dd8d093e3dfd6b10da1833c22eeaff9a880c84aa
eric <ericshell2010@gmail.com>
2016-04-22 22:14:40 -0400

    refactor

---
+++ client/app/_config/config.js
@@ -0,0 +1,17 @@
+(function () {
+    "use strict";
+
+    // angular.module('todos').config(AppConfig);
+    //
+    // AppConfig.$inject = ['$compileProvider'];
+    //
+    //
+    // function AppConfig($compileProvider){
+    //
+    //     // uncomment for production
+    //     $compileProvider.debugInfoEnabled(false);
+    //
+    // }
+
+
+})();
